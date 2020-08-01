376bedf019f1acf96a9f16d4f19e147dee0cb71d
Tyler Sargent <tsargent@gmail.com>
2020-08-01 17:11:12 -0400

    load posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+import axios from "axios";
+
+function Posts() {
+  // https://jsonplaceholder.typicode.com/posts
+  const postsQuery = useQuery("posts", () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(({ data }) => data)
+  );
+  console.log(postsQuery);
+  return (
+    <div>
+      {postsQuery.isLoading
+        ? "loading..."
+        : postsQuery.data.map((post) => <div>{post.title}</div>)}
+    </div>
+  );
+}
+
+function Post() {
+  // https://jsonplaceholder.typicode.com/posts/1
+}
 
 function App() {
+  const [postId, setPostId] = useState(-1);
   return (
-    <div className="App">
+    <div>
+      <button onClick={() => setPostId(postId - 1)}>Prev Post</button>
+      <button onClick={() => setPostId(postId + 1)}>Next Post</button>
+      <Posts />
+      <ReactQueryDevtools />
     </div>
   );
 }
